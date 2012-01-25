d7e996837b641e31e1e95c5ce62e1093ae8fa2fb
Tom Pesman <tom@tnux.net>
2012-01-25 22:35:45 +0100

    fix for sqlite db migration, index is too long

--- lib/generators/templates/add_c2dm_to_rapns_notifications.rb
+++ lib/generators/templates/add_c2dm_to_rapns_notifications.rb
@@ -1,5 +1,7 @@
 class AddC2dmToRapnsNotifications < ActiveRecord::Migration
   def self.up
+    # remove index for sqlite migration (index name is too long; the limit is 64 characters)
+    remove_index :rapns_notifications, :name => "index_rapns_notifications_on_delivered_failed_deliver_after"
     change_table :rapns_notifications do |t|
       t.string    :collapse_key,    :null => true
       t.string    :type,            :null => false, :default => "apns"
@@ -10,9 +12,11 @@ class AddC2dmToRapnsNotifications < ActiveRecord::Migration
     # - http://stackoverflow.com/questions/3868703/android-c2dm-registration-id-max-size
     # - http://groups.google.com/group/android-c2dm/browse_thread/thread/0141a5be624fc8c3
     change_column :rapns_notifications, :device_token, :string, :limit => 255
+
     # remove default value
     # can't add column to table with :null => false and no default, but you can change it afterwards
     change_column :rapns_notifications, :type, :string, :null => false, :default => "NULL"
+    add_index :rapns_notifications, [:delivered, :failed, :deliver_after], :name => "index_rapns_notifications_on_delivered_failed_deliver_after"
   end
 
   def self.down
